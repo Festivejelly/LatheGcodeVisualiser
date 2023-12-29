b18c566e3633bc001c3b6b69b007782eaa68717e
Nicholas John <NicholasIvorJohn@gmail.com>
2023-12-29 23:44:15 +0000

    Fix vite config to bundle assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vitest/config'
+// vite.config.ts
+import { defineConfig } from 'vite';
+import { viteSingleFile } from 'vite-plugin-singlefile';
 
 export default defineConfig({
+    plugins: [viteSingleFile()],
     build: {
         outDir: 'docs'
-    }
-})
+    },
+    // ... any other configurations ...
+});
