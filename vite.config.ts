6a3adaee224435ab50d642a1b7a270c5840f135b
Nicholas John <NicholasIvorJohn@gmail.com>
2025-05-27 17:46:30 +0100

    Removing google cloud stuff

--- vite.config.ts
+++ vite.config.ts
@@ -10,12 +10,5 @@ export default defineConfig({
     build: {
         outDir: 'docs'
     },
-    server: {
-        headers: {
-            'Cross-Origin-Opener-Policy': 'same-origin',
-            'Cross-Origin-Resource-Policy': 'same-origin',
-            'Content-Security-Policy': "script-src 'self' 'unsafe-inline' 'unsafe-eval' blob: data: https://www.gstatic.com;"
-        },
-    },
     // ... any other configurations ...
 });
